ad2e1916a69c8fa091fc6f109b312cc72d701816
ank1ta-rAj <adityapandey19850828@gmail.com>
2026-07-25 07:34:14 +0530

    Connect frontend to deployed backend

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
     setAnswer("");
 
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API}/ask`, {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/ask`, {
         question,
       });
 
